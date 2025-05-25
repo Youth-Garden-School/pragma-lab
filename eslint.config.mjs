a44f3ae1a5b2117027f94aff84e4a03c8d8f17b2
Quantaphocpython <quan02042004@gmail.com>
2025-05-25 14:42:24 +0700

    feat: set up lint commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,81 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import { createRequire } from 'module';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const require = createRequire(import.meta.url);
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
+
+// Import các plugin
+const pluginTs = require('@typescript-eslint/eslint-plugin');
+const tsParser = require('@typescript-eslint/parser');
+const pluginImport = require('eslint-plugin-import');
+const pluginPrettier = require('eslint-plugin-prettier');
+const pluginReact = require('eslint-plugin-react');
+const pluginReactHooks = require('eslint-plugin-react-hooks');
+const pluginUnusedImports = require('eslint-plugin-unused-imports');
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  resolvePluginsRelativeTo: __dirname,
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+/** @type {import("eslint").Linter.FlatConfig[]} */
+const config = [
+  js.configs.recommended,
+  {
+    ignores: ['./scripts/*'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': pluginTs,
+    },
+  },
+  ...compat.extends(
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'next',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended'
+  ),
+
+  {
+    plugins: {
+      react: pluginReact,
+      'react-hooks': pluginReactHooks,
+      import: pluginImport,
+      prettier: pluginPrettier,
+      'unused-imports': pluginUnusedImports,
+    },
+    rules: {
+      'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/exhaustive-deps': 'warn',
+      'import/no-unresolved': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'prettier/prettier': 'warn',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-magic-numbers': [
+        'warn',
+        {
+          ignore: [0, 1, 2],
+          ignoreArrayIndexes: true,
+          enforceConst: true,
+          detectObjects: false,
+        },
+      ],
+    },
+  },
 ];
 
-export default eslintConfig;
+export default config;
